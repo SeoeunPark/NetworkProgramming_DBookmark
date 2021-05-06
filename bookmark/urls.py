05abad0240e2cb0ba6eebe401baa98d22c799464
s2019w29 <s2019w29@e-mirim.hs.kr>
2021-05-06 09:53:46 +0900

    bookmark/urls bookmark:delete

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import BookmarkListView, BookmarkCreateView, BookmarkDetailView, BookmarkUpdateView
+from .views import BookmarkListView, BookmarkCreateView, BookmarkDetailView, BookmarkUpdateView, BookmarkDeleteView
 
 app_name = 'bookmark'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'),  # {% url 'bookmark:list' %}
     path('add/', BookmarkCreateView.as_view(), name='add'), # {% url 'bookmark:add' %}
     path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'), # {% url 'bookmark:detail' pk=bookmark.pk%}
-    path('update/<int:pk>/', BookmarkUpdateView.as_view(), name='update'),  # {% url 'bookmark:update' pk=bookmark.pk%}
+    path('update/<int:pk>/', BookmarkUpdateView.as_view(), name='update'),  # {% url 'bookmark:update' pk=bookmark.pk%}]
+    path('delete/<int:pk>/',BookmarkDeleteView.as_view(), name='delete')
 ]
